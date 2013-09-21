accf6c36132965c69c349e38ff01f9c6b89a7887
Carrotman42 <kevin@chowski.com>
2013-09-21 04:55:06 -0400

    Fixed names to be public

--- server/dt/datatypes.go
+++ server/dt/datatypes.go
@@ -4,34 +4,34 @@ package dt
 // This file represents the datatypes that are important to this project.
 
 type Mood struct {
-	r, g, b int
+	R, G, B int
 }
 func (m Mood) Dist(o Mood) float64 {
-	r, g, b := float64(m.r-o.r), float64(m.g-o.g), float64(m.b-o.b)
+	r, g, b := float64(m.R-o.R), float64(m.G-o.G), float64(m.B-o.B)
 	
 	return math.Sqrt(r*r + g*g + b*b)
 }
 
 type User struct {
-	id int
-	name string
-	curMood Mood
+	Id int
+	Name string
+	CurMood Mood
 }
 
 // Represents a set of users in a room
 type Room struct {
-	inRoom map[string]*User
+	InRoom map[string]*User
 }
 func (r Room) AverageMood() Mood {
 	r, g, b := 0.0, 0.0, 0.0
-	for _, v := range r.inRoom {
-		m := v.curMood
-		r += m.r
-		g += m.g
-		b += m.g
+	for _, v := range r.InRoom {
+		m := v.CurMood
+		r += m.R
+		g += m.G
+		b += m.B
 	}
 	
-	l := len(r.inRoom)
+	l := len(r.InRoom)
 	return Mood{int(r/l), int(g/l), int(b/l)}
 }
 
