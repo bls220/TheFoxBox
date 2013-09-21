93838b010d82b3e4728f105c9f71fbb41dc5bd11
Ben Smith <bls220@vt.edu>
2013-09-21 04:30:24 -0400

    Finished implementing SongAdapter

---
+++ android/TheFoxBox/src/com/team_awesome/thefoxbox/SongItem.java
@@ -0,0 +1,100 @@
+package com.team_awesome.thefoxbox;
+
+public class SongItem {
+
+	private String mAlbum;
+	private String mTitle;
+	private String mArtist;
+	private EVote mVote;
+
+	public static enum EVote {
+		DOWN(-1), NONE(0), UP(1);
+		private final int id;
+
+		EVote(int id) {
+			this.id = id;
+		}
+		
+		EVote(){
+			this.id = 0;
+		}
+
+		public int value() {
+			return id;
+		}
+	};
+
+	public SongItem() {
+		this("No Album","No Title","No Artist",EVote.NONE);
+	}
+
+	/**
+	 * @param mAlbum
+	 * @param mTitle
+	 * @param mArtist
+	 */
+	public SongItem(String mAlbum, String mTitle, String mArtist, EVote mVote) {
+		super();
+		this.mAlbum = mAlbum;
+		this.mTitle = mTitle;
+		this.mArtist = mArtist;
+		this.mVote = mVote;
+	}
+	
+	/**
+	 * @return the mAlbum
+	 */
+	public String getAlbum() {
+		return mAlbum;
+	}
+
+	/**
+	 * @param mAlbum the mAlbum to set
+	 */
+	public void setAlbum(String mAlbum) {
+		this.mAlbum = mAlbum;
+	}
+
+	/**
+	 * @return the mTitle
+	 */
+	public String getTitle() {
+		return mTitle;
+	}
+
+	/**
+	 * @param mTitle the mTitle to set
+	 */
+	public void setTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+
+	/**
+	 * @return the mArtist
+	 */
+	public String getArtist() {
+		return mArtist;
+	}
+
+	/**
+	 * @param mArtist the mArtist to set
+	 */
+	public void setArtist(String mArtist) {
+		this.mArtist = mArtist;
+	}
+
+	/**
+	 * @return the mVote
+	 */
+	public EVote getVote() {
+		return mVote;
+	}
+
+	/**
+	 * @param mVote the mVote to set
+	 */
+	public void setVote(EVote mVote) {
+		this.mVote = mVote;
+	}
+
+}
