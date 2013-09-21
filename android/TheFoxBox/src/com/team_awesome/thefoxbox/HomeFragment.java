0ab4b16fc3df2056bfb5768edb56d3b609b0b965
Ben Smith <bls220@vt.edu>
2013-09-21 10:28:41 -0400

    First contact established!

--- android/TheFoxBox/src/com/team_awesome/thefoxbox/HomeFragment.java
+++ android/TheFoxBox/src/com/team_awesome/thefoxbox/HomeFragment.java
@@ -3,8 +3,11 @@
  */
 package com.team_awesome.thefoxbox;
 
+import org.json.JSONException;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +16,7 @@ import android.view.ViewGroup;
  * @author bsmith
  *
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements QueryCallbacks {
 
 	/**
 	 * 
@@ -24,7 +27,40 @@ public class HomeFragment extends Fragment {
      public View onCreateView(LayoutInflater inflater, ViewGroup container,
              Bundle savedInstanceState) {
 		 View rootView = inflater.inflate(R.layout.fragment_nowplaying, container, false);
+		 
+		 Log.w(MainActivity.TAG,"Testing shiz");
+		 CommThread thread = new CommThread();
+		 try {
+			thread.login(this, "Ben");
+			thread.start();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		 
          return rootView;
 	 }
+
+	@Override
+	public void loginCallback(String authToken) {
+		Log.d(MainActivity.TAG,"AuthToken: " + authToken);		
+	}
+
+	@Override
+	public void queueCallback(FoxData data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void searchCallback(String[] results) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void submitCallback(String error) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
